c7085e074fd5b2e07395aa6a979cd333d6172ec5
Wolfgang Barth <Wolfgang.Barth@marienhaus.de>
2021-05-12 10:03:11 +0200

    server-configuration

---
+++ server-configuration.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'bundler/setup'
+Bundler.require(:development, :default)
+require 'mirth'
+
+Dotenv.load
+@cli = HighLine.new
+
+def get_user(prompt="Enter Login")
+  ENV['USER'] || @cli.ask(prompt) { |q| q.default = 'api' }
+end
+
+def get_passwd(prompt="Enter Password")
+  ENV['PASSWD'] || @cli.ask(prompt) { |q| q.echo = false }
+end
+
+def get_url(prompt="Enter URL")
+  ENV['URL'] || @cli.ask(prompt) { |q| q.default = 'https://localhost:8443/api' }
+end
+
+
+options = {
+  url: get_url,
+  ssl: { verify: false },
+}
+
+mapi = Mirth::Api.new(options)
+unless mapi.login(get_user, get_passwd)
+  puts "no valid session found, sorry"
+  exit
+end
+
+response = mapi.get("server/configuration")
+
+if response.success?
+  puts response.body
+else
+  puts response.error_messages.join("; ")
+end
+
+mapi.logout
